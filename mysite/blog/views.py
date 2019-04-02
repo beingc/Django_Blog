2bc93d8ddd568bfdfe404ce2101667042e549d7c
x <xuyang.x@qq.com>
2019-04-02 23:29:09 +0800

    complete pagination

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -49,6 +49,64 @@ class IndexView(ListView):
     context_object_name = 'post_list'
     paginate_by = 2
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        paginator = context.get('paginator')
+        page = context.get('page_obj')
+        is_paginated = context.get('is_paginated')
+
+        pagination_data = self.pagination_data(paginator, page, is_paginated)
+        context.update(pagination_data)
+        return context
+
+    def pagination_data(self, paginator, page, is_paginated):
+        if not is_paginated:
+            return {}
+        left = []
+        right = []
+        left_has_more = False
+        right_has_more = False
+        first = False
+        last = False
+        page_number = page.number
+        total_pages = paginator.num_pages
+        page_range = paginator.page_range
+
+        if page_number == 1:
+            right = page_range[page_number:page_number + 2]
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+            if right[-1] < total_pages:
+                last = True
+
+        elif page_number == total_pages:
+            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
+            if left[0] > 2:
+                left_has_more = True
+            if left[0] > 1:
+                first = True
+        else:
+            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0: page_number - 1]
+            right = page_range[page_number:page_number + 2]
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+            if right[-1] < total_pages:
+                last = True
+            if left[0] > 2:
+                left_has_more = True
+            if left[0] > 1:
+                first = True
+
+        data = {
+            'left': left,
+            'right': right,
+            'left_has_more': left_has_more,
+            'right_has_more': right_has_more,
+            'first': first,
+            'last': last,
+        }
+        return data
+
 
 class CategoryView(IndexView):
     def get_queryset(self):
@@ -84,7 +142,7 @@ class PostDetailView(DetailView):
     def get_object(self, queryset=None):
         post = super(PostDetailView, self).get_object(queryset=None)
         post.body = markdown.markdown(post.body,
-                                      extensions =[
+                                      extensions=[
                                           'markdown.extensions.extra',
                                           'markdown.extensions.codehilite',
                                           'markdown.extensions.toc',
@@ -96,7 +154,7 @@ class PostDetailView(DetailView):
         form = CommentForm()
         comment_list = self.object.comment_set.all()
         context.update({
-            'form':form,
-            'comment_list':comment_list
+            'form': form,
+            'comment_list': comment_list
         })
         return context
